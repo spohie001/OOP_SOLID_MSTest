f25756990c729ba8eef812af96a3bd9a45a3c816
spohie001 <z.sosinska001@gmail.com>
2025-06-28 10:46:47 +0200

    violating SRP

---
+++ OOP_SOLID/SPLOD/SingleResponsibility/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_SOLID.SPLOD.SingleResponsibility
+{
+    internal class EmailSender
+    {
+        public string SendEmail(string email, string msg)
+        {
+            return $"Sending email to {email}: {msg}";
+        }
+    }
+}
---
+++ OOP_SOLID/SPLOD/SingleResponsibility/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_SOLID.SPLOD.SingleResponsibility
+{
+    //"A class should have only one reason to change. meaning that it hould have only one responsibility or purpose
+    
+    internal class User //class violates SRP!
+    {
+        //it manages user data
+        public string Username {  get; set; } //* 1. 
+        public string Email { get; set; }
+
+        //and also containes logic for registration
+        public void Register()
+        {
+            //business logic..
+
+            //Send email
+            var emailSender = new EmailSender();
+            emailSender.SendEmail(this.Email, "Welcome to our platform");
+        }
+
+        //* reazons to be changed:
+        //* 1. we need more fields
+        //* 2. wen want to change logic of the registration process
+    }
+}
