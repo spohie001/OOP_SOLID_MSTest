b3c2ae3873b11e2596b4311653f589c2668f1464
spohie001 <z.sosinska001@gmail.com>
2025-06-28 11:11:24 +0200

    violating OCP

---
+++ OOP_SOLID/SPLOD/OpenClosed/ShapeType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_SOLID.SPLOD.OpenClosed
+{
+    internal enum ShapeType
+    {
+        Circle,
+        Rectangle
+    }
+}
