3bcb43b2c65ddc3b36b304b2625c7dcee7b684d4
spohie001 <z.sosinska001@gmail.com>
2025-06-28 13:28:00 +0200

    violating DIP

---
+++ OOP_SOLID/SPLOD/DependencyInversion/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_SOLID.SPLOD.DependencyInversion
+{
+    internal class Car
+    {
+        private Engine _engine;
+
+        public Car()
+        {
+            _engine = new Engine(); //if we want to change type of engine we have to change source code
+        }
+        public void StartCar()
+        {
+            _engine.Start();
+        }
+    }
+}
