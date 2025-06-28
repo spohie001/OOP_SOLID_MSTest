b487ac1fdb65ee888f4b3780c7c76742963caf50
spohie001 <z.sosinska001@gmail.com>
2025-06-28 10:51:22 +0200

    Single Responsibility Principle

--- OOP_SOLID/SPLOD/SingleResponsibility/User.cs
+++ OOP_SOLID/SPLOD/SingleResponsibility/User.cs
@@ -9,24 +9,10 @@ namespace OOP_SOLID.SPLOD.SingleResponsibility
 {
     //"A class should have only one reason to change. meaning that it hould have only one responsibility or purpose
     
-    internal class User //class violates SRP!
+    internal class User //user has only one responsibility
     {
-        //it manages user data
-        public string Username {  get; set; } //* 1. 
+        public string Username {  get; set; }
         public string Email { get; set; }
 
-        //and also containes logic for registration
-        public void Register()
-        {
-            //business logic..
-
-            //Send email
-            var emailSender = new EmailSender();
-            emailSender.SendEmail(this.Email, "Welcome to our platform");
-        }
-
-        //* reazons to be changed:
-        //* 1. we need more fields
-        //* 2. wen want to change logic of the registration process
     }
 }
---
+++ OOP_SOLID/SPLOD/SingleResponsibility/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_SOLID.SPLOD.SingleResponsibility
+{
+    internal class UserService
+    {
+        public string Register(User user) //buissness logic is now in a different class
+        {
+            //business logic..
+
+            //Send email
+            var emailSender = new EmailSender();
+            return emailSender.SendEmail(user.Email, "Welcome to our platform");
+        }
+    }
+}
